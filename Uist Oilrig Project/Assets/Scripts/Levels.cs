dbc3545861e9300df65e2f40b8a79372de217803
Skyler16 <64692388+Skyler16@users.noreply.github.com>
2020-10-23 03:49:25 +0800

    Add start, credits and level select scenes

---
+++ Uist Oilrig Project/Assets/Scripts/Levels.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Levels : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelSelection( int level)
+    {
+        switch (level)
+        {
+            case 1: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            case 2: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            case 3: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            case 4: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            case 5: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            case 6: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            //case 7: SceneManager.LoadScene("OilRigSample_AddPlayerMovement"); break;
+            default:  break;
+        }
+    }
+}
---
+++ Uist Oilrig Project/Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            LoadStartScene();
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("OilRigStart");
+    }
+
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("OilRigCredits");
+    }
+
+    public void LoadLevelScene()
+    {
+        SceneManager.LoadScene("OilRigLevelSelect");
+    }
+}
