f7d034e647b4c6e27ec91549142271f4a0f6f6db
Sandy Steele <Sandy_steele@hotmail.com>
2020-10-23 00:17:12 +0100

    Decom Test
    Added a scene to test a decomissioning minigame.

---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/Game 1/Grabber.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(DistanceJoint2D))]
+public class Grabber : MonoBehaviour
+{
+    DistanceJoint2D joint;
+
+    public float depth;
+
+    float maxHeight;
+    float minHeight;
+    public float grabDistance;
+    int time = 0;
+    int maxTime = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = this.GetComponent<DistanceJoint2D>();
+
+        maxHeight = transform.position.y;
+        minHeight = transform.position.y - depth;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Fire1") > 0)
+        {
+            time++;
+        } else
+        {
+            time--;
+        }
+
+        //Time is bounded by 0 and maxTime.
+        if (time < 0)
+        {
+            time = 0;
+        }
+        if (time > maxTime)
+        {
+            time = maxTime;
+        }
+
+        float height = maxHeight - ((float)time / maxTime) * maxHeight;
+
+        transform.position = new Vector3(transform.position.x, height, transform.position.z);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Trigger")
+        {
+            joint.connectedBody = null;
+        }
+        if (collision.GetComponent<Rigidbody2D>())
+        {
+            joint.connectedBody = collision.GetComponent<Rigidbody2D>();
+            Debug.Log("Done it");
+        }
+    }
+}
---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/Game 1/Pickup_Entity.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup_Entity : MonoBehaviour
+{
+
+    DistanceJoint2D joint;
+
+    public float leftX;
+    public float rightX;
+
+    private float slopeCalc;
+
+    public float maxHorSpeed;
+    public float maxVertSpeed;
+
+    public GameObject Claw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = this.GetComponentInChildren<DistanceJoint2D>();
+        slopeCalc = (rightX - leftX) / (Screen.width);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPos = Input.mousePosition;
+        float targetX = leftX + slopeCalc * (targetPos.x - 0);
+
+        if(targetX - transform.position.x > maxHorSpeed)
+        {
+            targetX = transform.position.x + maxHorSpeed;
+        }
+        else if (targetX - transform.position.x < -maxHorSpeed)
+        {
+            targetX = transform.position.x - maxHorSpeed;
+        }
+        transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+    }
+}
