3cd999f88969f09ae8ae103f40843d3aaf88fc23
Adetoddle <72755460+Adetoddle@users.noreply.github.com>
2020-10-30 08:20:39 +0800

    add navigation icon
    also show distance

---
+++ Uist Oilrig Project/Assets/Scripts/UI/Waypoint.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Waypoint : MonoBehaviour
+{
+    public RectTransform prefab;
+    private RectTransform waypoint;
+    private Transform player;
+    private Text distanceText;
+    private Vector3 offset = new Vector3(0, 1f, 0);
+   
+    void Start()
+    {
+        var canvas = GameObject.Find("Waypoints").transform;
+        waypoint=Instantiate(prefab, canvas);
+        player = GameObject.Find("Player").transform;
+        distanceText = waypoint.GetComponentInChildren<Text>();
+      //  distanceText = waypoint.GetComponent<Text>();
+    }
+
+    
+    void Update()
+    {
+        
+
+
+        var screenPos = Camera.main.WorldToScreenPoint(transform.position + offset);
+        waypoint.position = screenPos;
+        distanceText.text = Vector3.Distance(player.position, transform.position).ToString("0") + "m";
+        waypoint.gameObject.SetActive(screenPos.z > 0);
+    }
+}
