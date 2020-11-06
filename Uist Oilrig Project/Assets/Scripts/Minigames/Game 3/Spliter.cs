20479ed965afd27169aa63c3eb35524071fa1d62
Adetoddle <72755460+Adetoddle@users.noreply.github.com>
2020-11-06 08:07:59 +0800

    added minigame 3
    Implemented minigame3， added a drill to cut certain objects.

---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/Game 3/Spliter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;
+public class Spliter : MonoBehaviour
+{
+    public Material matCross;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider[] colliders = Physics.OverlapBox(transform.position, new Vector3(0.5f, 0.005f, 0.5f),transform.rotation,~LayerMask.GetMask("solid","drill"));
+            foreach (Collider c in colliders)
+            {
+                Destroy(c.gameObject);
+                //GameObject[] objs = c.gameObject.SliceInstantiate(transform.position, transform.up);
+                SlicedHull hull = c.gameObject.Slice(transform.position, transform.up);
+                if (hull != null)
+                {
+                    GameObject lower = hull.CreateLowerHull(c.gameObject, matCross);
+                    GameObject upper = hull.CreateLowerHull(c.gameObject, matCross);
+                    GameObject[] objs = new GameObject[] { lower, upper };
+                    foreach (GameObject obj in objs)
+                    {
+                        Rigidbody rb= obj.AddComponent<Rigidbody>();
+                        obj.AddComponent<MeshCollider>().convex = true;
+                        rb.AddExplosionForce(1, c.gameObject.transform.position, 1);
+                    }
+                }
+                
+            }
+        }
+    }
+}
