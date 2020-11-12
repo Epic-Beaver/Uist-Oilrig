40cb388510617236bf85fc6e9ed932e58e41854c
Epic-Beaver <39735721+Epic-Beaver@users.noreply.github.com>
2020-11-12 16:40:56 +0000

    Updated the rain prefab
    Added a rain controler and script, added fog interaction. (proper explination in programming general)

---
+++ Uist Oilrig Project/Assets/Scripts/WeatherControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherControler : MonoBehaviour
+{
+
+    public bool goodWeather = true;
+
+    public bool allowFog = true;
+    private ParticleSystem ps;
+    public float fogDensity = 0.1f;
+
+    public Material goodSky;
+    public Material badSky;
+
+    void Start()
+    {
+        ps = gameObject.GetComponent<ParticleSystem>();
+        updateWeather();
+    }
+
+
+    void updateWeather()
+    {
+        if (goodWeather)
+        {
+            setGoodWeather();
+        }
+        else
+        {
+            setBadWeather();
+        }
+    }
+
+    void setBadWeather()
+    {
+        RenderSettings.fog = allowFog;
+        var emission = ps.emission;
+        emission.enabled = true;
+        RenderSettings.skybox = badSky;
+    }
+
+    void setGoodWeather()
+    {
+        RenderSettings.fog = false;
+        var emission = ps.emission;
+        emission.enabled = false;
+        ps.Clear();
+        RenderSettings.skybox = goodSky;
+    }
+}
