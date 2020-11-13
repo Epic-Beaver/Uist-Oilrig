f669766df09f9651ec7fc30c0f76a97f83f4fd3b
Adetoddle <72755460+Adetoddle@users.noreply.github.com>
2020-11-13 14:19:52 +0800

    finished and ompitimized planningStage
    still have some problem with choosing the wrong order.
    which means if chose the right order, everthing was fine

--- Uist Oilrig Project/Assets/Scripts/PlanningStage/Drag.cs
+++ Uist Oilrig Project/Assets/Scripts/PlanningStage/Drag.cs
@@ -1,126 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour //,IBeginDragHandler,IEndDragHandler,IDragHandler
+public class Drag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    public static event Action PuzzleDone = delegate { };
-  //  [SerializeField]
-  //  private Canvas canvas; 
-  //  private RectTransform rectTransform;
-  //  private CanvasGroup canvasGroup;
+
     [SerializeField]
-    private Transform standPosition;
-    private Vector2 initialPosition;
-    private Vector2 mousePosition;
-    private float deltaX, deltaY;
-    public static  bool locked;
-    
-    void Start()
-    {  
-        initialPosition = transform.position;
-        locked = false;
+    private Canvas canvas;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    [SerializeField]
+    private static Transform correctTrans;
 
-    }
-
-    /*  private void Awake()
-      {
-          canvasGroup = GetComponent<CanvasGroup>();
-          rectTransform = GetComponent<RectTransform>();
-      }
-
-       public void OnBeginDrag(PointerEventData eventData)
-       {
-
-           transform.position = eventData.position;
-           GetComponent<CanvasGroup>().blocksRaycasts = false;
-
-       }
-
-       public void OnDrag(PointerEventData eventData)
-       {
-
-           transform.position = eventData.position;
-           rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
-           Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
-
-       }
-
-       public void OnEndDrag(PointerEventData eventData)
-       {
-           if (eventData.pointerCurrentRaycast.gameObject.name == "1")
-           {
-               transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
-               GetComponent<CanvasGroup>().blocksRaycasts = true;
-           }
+    //public static Vector2 initialPosition;
 
 
-       }*/
+    //void Start()
+    //{  
+    //    initialPosition = rectTransform.anchoredPosition;
 
-    private void OnMouseDown()
+    //}
+
+    private void Awake()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("aaa");
-            deltaX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x;
-            deltaY = Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y;
-
-
-        }
+        canvasGroup = GetComponent<CanvasGroup>();
+        rectTransform = GetComponent<RectTransform>();
     }
-   
-    private void OnMouseDrag()
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        if (!locked)
-        {
-            Debug.Log("bbb");
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector2(mousePosition.x - deltaX, mousePosition.y - deltaY);
-        }
+
+        // transform.position = eventData.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
     }
-    private void OnMouseUp()
+
+    public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("ddd");
-        if (Mathf.Abs(transform.position.x - standPosition.position.x) <= 1f && Mathf.Abs(transform.position.y - standPosition.position.y) <= 5f)
-        {
-            switch (PlanningManager.slotOccupoied)
-            {
-                case 0:
-                    transform.position = new Vector2(transform.position.x, 114f);
-                    PlanningManager.slotOccupoied = 1;
-                    break;
-                case 1:
-                    transform.position = new Vector2(transform.position.x, 41f);
-                    PlanningManager.slotOccupoied = 2;
-                    break;
-                case 2:
-                    transform.position = new Vector2(transform.position.x, -26.5f);
-                    PlanningManager.slotOccupoied = 3;
-                    break;
-                case 3:
-                    transform.position = new Vector2(transform.position.x, -106f);
-                    PlanningManager.slotOccupoied = 4;
-                    break;
-                case 4:
-                    transform.position = new Vector2(transform.position.x, -180f);
-                    PlanningManager.slotOccupoied = 5;
-                    break;
-                case 5:
-                    transform.position = new Vector2(transform.position.x, -256f);
-                    PuzzleDone();
-                    break;
-            }
-            locked = true;
-        }
-        else
-        {
-            transform.position = new Vector2(initialPosition.x, initialPosition.y);
-        }
+
+        transform.position = eventData.position;
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        Debug.Log(eventData.pointerCurrentRaycast.gameObject.name);
+        Debug.Log(eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition);
 
     }
 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+        //      transform.position = eventData.pointerCurrentRaycast.gameObject.transform.position;
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
 
 
+
+    }
 }
--- Uist Oilrig Project/Assets/Scripts/PlanningStage/PlanningManager.cs
+++ Uist Oilrig Project/Assets/Scripts/PlanningStage/PlanningManager.cs
@@ -5,36 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class PlanningManager : MonoBehaviour
 {
-    public GameObject[] Icons,Scenes;
-    public static int slotOccupoied;
+    public GameObject[] Icons, Scenes, box;
     [SerializeField]
     private Transform[] options;
     [SerializeField]
     private GameObject wrongSign;
 
+
+
     private void Start()
     {
-        Drag.PuzzleDone += CheckResults;
-        slotOccupoied = 0;
+
+    }
+    void Update()
+    {
+        CheckResults();
+
     }
     public void CheckResults()
     {
-        if(options[0].position.y == 114 && options[1].position.y == 41 && options[2].position.y == -26.5 && options[3].position.y == -106 && options[4].position.y == -180 && options[5].position.y == -256)
+        Debug.Log("check");
+        for (int i = 0; i < 6; i++)
+        {
+            //    if (options[i].position == options[i].correcttrans.position)
+            {
+                //        scenes[2].setactive(false);
+                //        scenes[3].setactive(true);
+                //        debug.log("win");
+            }
+        }
+
+        if (options[0].position == box[0].transform.position && options[1].position == box[1].transform.position && options[2].position == box[2].transform.position && options[3].position == box[3].transform.position && options[4].position == box[4].transform.position && options[5].position == box[5].transform.position)
         {
             Scenes[2].SetActive(false);
             Scenes[3].SetActive(true);
             Debug.Log("win");
         }
-        else
-        {
-            Debug.Log("lost");
-            wrongSign.SetActive(true);
-            Invoke("ReloadGame", 1f);
-        }
+
+        //else 
+
+        //{
+        //    Debug.Log("lost");
+
+        //    wrongSign.SetActive(true);
+        //    Invoke("ReloadGame", 1f);
+        //}
+
     }
     public void ReloadGame()
     {
-        Drag.PuzzleDone -= CheckResults;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void Win()
@@ -42,29 +61,23 @@ public class PlanningManager : MonoBehaviour
         if (Icons[0].activeSelf == true && Icons[2].activeSelf == true && Icons[3].activeSelf == true && Icons[5].activeSelf == true && Icons[6].activeSelf == true && Icons[9].activeSelf == true)
         {
             Scenes[0].SetActive(false);
-          
+
             Scenes[1].SetActive(true);
-            
+
         }
     }
     public void ChangeScenes()
     {
-       
+
         Scenes[1].SetActive(false);
         Scenes[2].SetActive(true);
-    }
-    public void ChangeScenes1()
-    {
-
-        Scenes[2].SetActive(false);
-        Scenes[3].SetActive(true);
     }
     public void Finish()
     {
         SceneManager.LoadScene("BuildStart");
     }
-  
-   
+
+
 
 
 }
