226cde845f089138dbe7183122e0405d72c6591d
Sandy Steele <Sandy_steele@hotmail.com>
2020-11-17 11:03:26 +0000

    Basic point and click implementation
    In "Spinning Decom" the players can now remove segments in order.

---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/Game 5/ClickRemove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickRemove : MonoBehaviour
+{
+    public Material regularMat;
+    public Material highlightMat;
+
+    public ClickRemove[] preRequisites;
+
+    public string sectionName;
+
+    public bool removed = false;
+
+    public MouseOverText mouseOver;
+    public bool hover;
+
+    private MeshRenderer mesh;
+
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = this.GetComponent<MeshRenderer>();
+        anim = this.GetComponent<Animator>();
+
+        mesh.material = regularMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        hover = true;
+        mouseOver.updateInfo(name, this);
+        mesh.material = highlightMat;
+
+        //MouseOverText
+    }
+
+    private void OnMouseExit()
+    {
+        hover = false;
+        mesh.material = regularMat;
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (ClickRemove pre in preRequisites)
+        {
+            if(!pre.removed)
+            {
+                //Cannot remove, inform player.
+
+                return;
+            }
+        }
+
+        anim.SetTrigger("Remove");
+        removed = true;
+    }
+}
---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/Game 5/MouseOverText.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOverText : MonoBehaviour
+{
+    public string information = "";
+
+    private Image img;
+    private Text uiText;
+    private ClickRemove last;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = this.GetComponentInChildren<Image>();
+        uiText = this.GetComponentInChildren<Text>();
+        last = FindObjectOfType<ClickRemove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (last.hover == false)
+        {
+            img.enabled = false;
+            information = "";
+        } else
+        {
+            img.enabled = true;
+        }
+
+        uiText.text = information;
+        transform.position = Input.mousePosition;
+    }
+
+    public void updateInfo(string information, ClickRemove last)
+    {
+        this.information = information;
+        this.last = last;
+    }
+}
