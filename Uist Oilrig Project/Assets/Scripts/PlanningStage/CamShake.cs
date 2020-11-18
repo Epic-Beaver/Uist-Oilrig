fa86ed00522ab4d1d56f9fcc03311a165302d806
Adetoddle <72755460+Adetoddle@users.noreply.github.com>
2020-11-18 21:30:48 +0800

    modify planning stage

---
+++ Uist Oilrig Project/Assets/Scripts/PlanningStage/CamShake.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamShake : MonoBehaviour
+{
+    //public Vector3 objectPos;
+
+    //public Vector3 strength;
+    //private Vector3 strengthDefault;
+
+    //public float shakeTime = 1f;
+    //private float shakeTimeDefault;
+
+    //public float decay = 0.8f;
+
+    //public bool isShaking = false;
+
+    //private void Start()
+    //{
+    //    objectPos = transform.position;
+    //    strengthDefault = strength;
+    //    shakeTimeDefault = shakeTime;
+    //}
+    //private void Update()
+    //{
+    //    if (isShaking)
+    //    {
+    //        if (shakeTime > 0)
+    //        {
+    //            shakeTime -= Time.deltaTime;
+    //            Vector3 temPos = transform.position;
+    //            temPos.x = objectPos.x + Random.Range(-strength.x, strength.x);
+    //            temPos.y = objectPos.y + Random.Range(-strength.y, strength.y);
+    //            temPos.z = objectPos.z + Random.Range(-strength.z, strength.z);
+    //            transform.position = temPos;
+    //            strength *= decay;
+    //        }
+    //        else
+    //        {
+    //            shakeTime = 0;
+    //            transform.position = objectPos;
+    //            isShaking = false;
+    //            strength = strengthDefault;
+    //            shakeTime = shakeTimeDefault;
+    //        }
+    //    }
+    //}
+    //public void StartShake()
+    //{
+    //    isShaking = true;
+    //    strength = strengthDefault;
+    //    shakeTime = shakeTimeDefault;
+    //}
+
+    public Animator camAnim;
+    public void camshake()
+    {
+        camAnim.SetTrigger("shake");
+    }
+    
+    
+}
--- Uist Oilrig Project/Assets/Scripts/PlanningStage/PlanningManager.cs
+++ Uist Oilrig Project/Assets/Scripts/PlanningStage/PlanningManager.cs
@@ -1,28 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlanningManager : MonoBehaviour
 {
     public GameObject[] Icons, Scenes, box;
     [SerializeField]
-    private Transform[] options;
+    private Transform[] options, initialPosition;
     [SerializeField]
     private GameObject wrongSign;
-    
-   
+    public CamShake CamShake;
 
-    private void Start()
-    {
-
-    }
     void Update()
     {
 
         if (options[0].position.x == box[0].transform.position.x && options[1].position.x == box[1].transform.position.x && options[2].position.x == box[2].transform.position.x && options[3].position.x == box[3].transform.position.x && options[4].position.x == box[4].transform.position.x && options[5].position.x == box[5].transform.position.x)
         {
-            Debug.Log("success");
+            Debug.Log("CheckResultssuccess");
             CheckResults();
         }
 
@@ -38,37 +33,37 @@ public class PlanningManager : MonoBehaviour
 
         if (options[0].position == box[0].transform.position && options[1].position == box[1].transform.position && options[2].position == box[2].transform.position && options[3].position == box[3].transform.position && options[4].position == box[4].transform.position && options[5].position == box[5].transform.position)
         {
-            Scenes[2].SetActive(false);
-            Scenes[3].SetActive(true);
-            Debug.Log("win");
+            Scenes[3].SetActive(false);
+            Scenes[4].SetActive(true);
+            Debug.Log("rightorder");
         }
         else
         {
             Debug.Log("lost");
 
             wrongSign.SetActive(true);
+
             Invoke("ReloadGame", 1f);
         }
     }
     public void ReloadGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        wrongSign.SetActive(false);
+        for (int i = 0; i < 6; i++)
+        {
+            options[i].position = initialPosition[i].position;
+        }
     }
     public void Win()
     {
         if (Icons[0].activeSelf == true && Icons[2].activeSelf == true && Icons[3].activeSelf == true && Icons[5].activeSelf == true && Icons[6].activeSelf == true && Icons[9].activeSelf == true)
         {
-            Scenes[0].SetActive(false);
+            Debug.Log("win");
+            Scenes[1].SetActive(false);
 
-            Scenes[1].SetActive(true);
+            Scenes[2].SetActive(true);
 
         }
-    }
-    public void ChangeScenes()
-    {
-
-        Scenes[1].SetActive(false);
-        Scenes[2].SetActive(true);
     }
     public void Finish()
     {
