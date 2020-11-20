8849a74b3822b590a3ed10ffddae143e9d9abaf9
Sandy Steele <Sandy_steele@hotmail.com>
2020-11-20 11:51:29 +0000

    Penultimate push
    Just need to add the order to the minigames.
    Added in a small note next to each minigame for when you select it.

---
+++ Uist Oilrig Project/Assets/Scripts/Minigames/InfoTab.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoTab : MonoBehaviour
+{
+    [TextArea]
+    public string[] information;
+
+    public string[] sceneNames;
+
+    private Animator anim;
+
+    public SimpleSceneManage sceneLoader;
+
+    public Text textField;
+
+    private int index = 0;
+
+    private bool displaying = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Load into the currently selected scene
+    public void begin()
+    {
+        if (index > 0)
+        {
+            sceneLoader.SceneByName(sceneNames[index]);
+        }
+    }
+
+    public void updateInfoTab()
+    {
+        textField.text = information[index];
+    }
+
+    public void display(int index)
+    {
+
+        this.index = index;
+        if (displaying)
+        {
+            anim.SetTrigger("Hide");
+        }
+        else
+        {
+            updateInfoTab();
+        }
+
+        displaying = true;
+        anim.SetTrigger("Display");
+    }
+
+}
