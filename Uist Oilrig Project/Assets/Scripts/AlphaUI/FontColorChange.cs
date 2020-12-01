a02c583fb39fff01b1605d18b44f1fbb31fca05c
Skyler16 <64692388+Skyler16@users.noreply.github.com>
2020-12-01 20:25:49 +0800

    Updated planning stage
    Changed the color of book closing.
    Added a book turning sound.
    Mouse hover appearances style in the first part of the game.

--- Uist Oilrig Project/Assets/Scripts/AlphaUI/FontColorChange.cs
+++ Uist Oilrig Project/Assets/Scripts/AlphaUI/FontColorChange.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class FontColorChange : MonoBehaviour
+public class FontColorChange : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Text text;
+
+    private float r;
+    private float g;
+    private float b;
     // Start is called before the first frame update
     void Start()
     {
-        
+        r = this.GetComponent<Image>().color.r;
+        g = this.GetComponent<Image>().color.g;
+        b = this.GetComponent<Image>().color.b;
     }
 
     // Update is called once per frame
@@ -26,5 +33,15 @@ public class FontColorChange : MonoBehaviour
     public void AnswerWrong()
     {
         text.color = Color.red;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().color = new Color(r, g, b, 1);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponent<Image>().color = new Color(r, g, b, 0);
     }
 }
