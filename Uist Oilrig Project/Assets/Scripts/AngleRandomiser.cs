6384592f7c08935e7d27fc12726032c4c5067b51
Sandy Steele <Sandy_steele@hotmail.com>
2020-12-11 13:41:33 +0000

    Post Processing and Time of Day Fixes
    Implemented Post Processing features and implemented Time of Day.

---
+++ Uist Oilrig Project/Assets/Scripts/AngleRandomiser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleRandomiser : MonoBehaviour
+{
+
+    public Vector3 startAngle;
+    public Vector3 endAngle;
+
+    public bool randomPCT;
+
+    public float pct;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (randomPCT)
+        {
+            pct = Random.Range(0f, 1f);
+        }
+
+        transform.rotation = Quaternion.Euler(Vector3.Lerp(startAngle, endAngle, pct));
+    }
+}
