97ad705d9a62a362e4e423af66d0ca4e99787d4c
Epic-Beaver <39735721+Epic-Beaver@users.noreply.github.com>
2020-12-14 20:26:48 +0000

    Added volume Bar to all sceenes and normalized the volumes

---
+++ Uist Oilrig Project/Assets/Scripts/VolumeBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeBar : MonoBehaviour
+{
+    public int volumeMod = 20;
+    private void Start()
+    {
+        print("STARTING VOLUME " + AudioListener.volume + "\n");
+    }
+    public void changeVolume(float sliderValue)
+    {
+        print("VOLUME " + sliderValue + "\n");
+        AudioListener.volume = sliderValue;
+    }
+
+}
